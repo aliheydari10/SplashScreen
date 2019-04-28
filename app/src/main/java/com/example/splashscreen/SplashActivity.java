4a54e17838a8f97bc426cd8e1a9dbd9f60d269ce
Ali Heydari <alihydari0980@gmail.com>
2019-04-28 16:25:58 +0430

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.splashscreen;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    private MediaPlayer splashSound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        splashSound = MediaPlayer.create(this, R.raw.bell);
+        splashSound.start();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        },2600);
+    }
+}
